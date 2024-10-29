d10a26423b47fc73067d888667d20ee15ccf66bf
ndlorenc <ndlorenc@icloud.com>
2024-10-29 14:06:48 -0400

    [ndlorenc] Added test files and started tests_cv.py

---
+++ backend/tests_cv.py
@@ -0,0 +1,68 @@
+from unittest.main import main
+from flask import app
+import unittest
+
+class FlaskTest(unittest.TestCase):
+
+    def test_zero_shot_Charles_Lenovo(self):
+        return 0
+    
+    def test_zero_shot_Cynthia_Fidelity(self):
+        return 0
+    
+    def test_zero_shot_Ava_RedHat(self):
+        return 0
+    
+    def test_zero_shot_Charles_EpicGames(self):
+        return 0
+    
+    def test_zero_shot_Ava_Apple(self):
+        return 0
+    
+    def test_one_shot_Charles_Lenovo(self):
+        return 0
+    
+    def test_one_shot_Cynthia_Fidelity(self):
+        return 0
+    
+    def test_one_shot_Ava_RedHat(self):
+        return 0
+    
+    def test_one_shot_Charles_EpicGames(self):
+        return 0
+    
+    def test_one_shot_Ava_Apple(self):
+        return 0
+    
+    def test_one_shot_long_Charles_Lenovo(self):
+        return 0
+    
+    def test_one_shot_long_Cynthia_Fidelity(self):
+        return 0
+    
+    def test_one_shot_long_Ava_RedHat(self):
+        return 0
+    
+    def test_one_shot_long_Charles_EpicGames(self):
+        return 0
+    
+    def test_one_shot_long_Ava_Apple(self):
+        return 0
+    
+    def test_few_shot_Charles_Lenovo(self):
+        return 0
+    
+    def test_few_shot_Cynthia_Fidelity(self):
+        return 0
+    
+    def test_few_shot_Ava_RedHat(self):
+        return 0
+    
+    def test_few_shot_Charles_EpicGames(self):
+        return 0
+    
+    def test_few_shot_Ava_Apple(self):
+        return 0
+    
+if __name__=="__main__":
+    unittest.main()
