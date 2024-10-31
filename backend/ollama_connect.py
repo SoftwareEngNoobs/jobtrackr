d23d1ac1f48081e684c029b7c5b094f4478f130d
jfmcdavitt <mcdavittjacob@gmail.com>
2024-10-31 14:37:14 -0400

    Needed to fix files and ollama connect again to make sure it printed out suggestions

--- backend/ollama_connect.py
+++ backend/ollama_connect.py
@@ -12,7 +12,7 @@ Only respond with the cover letter and nothing else.
 
 system_prompt_suggest = """
 You are a helpful assistant that generates suggestions for how to update a resume from a resume and a job description.
-Only respond with the cover letter and nothing else.
+Only respond with the suggestions and nothing else.
 """
 
 example_files = []
